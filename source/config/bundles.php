1ce5b7a59dcb7e04e150785421a828ce9efda70f
Xavier Marchegay <xavier.marchegay@gmail.com>
2023-11-24 14:55:50 +0100

    add channel select

--- source/config/bundles.php
+++ source/config/bundles.php
@@ -60,4 +60,5 @@ return [
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
---
+++ source/src/Controller/ChannelController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ChannelController extends AbstractController
+{
+    public function __construct(
+        private readonly ChannelRepositoryInterface $channelRepository
+    )
+    {
+    }
+
+    public function listChannels(): Response
+    {
+        $channels = $this->channelRepository->findBy(criteria: [], orderBy: ['name' => 'asc']);
+
+        return $this->render('channel/list.html.twig', [
+            'channels' => $channels,
+        ]);
+    }
+}
