d57c83dd63717a3b44972c749b346f5b27d1dc1a
Xavier Marchegay <xavier.marchegay@gmail.com>
2023-11-25 14:56:29 +0100

    add cache to all products

--- source/src/Controller/ChannelController.php
+++ source/src/Controller/ChannelController.php
@@ -7,19 +7,28 @@ namespace App\Controller;
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ChannelController extends AbstractController
 {
     public function __construct(
         private readonly ChannelRepositoryInterface $channelRepository,
+        private readonly CacheInterface $cache,
     )
     {
     }
 
     public function listChannels(): Response
     {
+        $channels = $this->cache->get('channels_list', function (ItemInterface $item): array {
+            $item->expiresAfter(3600);
+
+            return $this->channelRepository->findBy(criteria: ['enabled' => true], orderBy: ['name' => 'asc']);
+        });
+
         return $this->render('channel/list.html.twig', [
-            'channels' => $this->channelRepository->findBy(criteria: ['enabled' => true], orderBy: ['name' => 'asc']),
+            'channels' => $channels,
         ]);
     }
 }
